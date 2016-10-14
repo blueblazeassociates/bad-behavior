f17562060ba1f3f620ace8b8861b6c1ce1fa9f7c
Ed Gifford <egifford@blueblazeassociates.com>
2016-10-14 14:28:16 -0400

    Bumping version to 2.2.19.2.

--- bad-behavior-wordpress.php
+++ bad-behavior-wordpress.php
@@ -20,7 +20,7 @@
  * Plugin URI:          https://github.com/blueblazeassociates/bad-behavior
  * Original Plugin URI: http://bad-behavior.ioerror.us/
  * Description:         Deny automated spambots access to your PHP-based Web site.
- * Version:             2.2.19.1
+ * Version:             2.2.19.2
  * Author:              Michael Hampton
  * Author URI:          http://bad-behavior.ioerror.us/
  * License:             LGPLv3
